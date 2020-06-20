5a313f095f324709604df4ff14f396dfb9752f2d
gh <120444790@qq.com>
2020-06-20 14:39:51 +0800

    feat: basic

---
+++ src/UserForm.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form, Input } from 'antd';
+
+interface Props {
+    form: any
+}
+class UserForm extends Component<Props> {
+    handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const { form: { getFieldsValue } } = this.props;
+        const values = getFieldsValue()
+        console.log(values, 'values');
+    }
+    render() {
+        const { form: { getFieldDecorator } } = this.props;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item label="username">
+                    {
+                        getFieldDecorator('username')(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item label="password">
+                    {
+                        getFieldDecorator('password')(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item>
+                    <button>提交</button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+const wrappedUserForm = Form.create()(UserForm);
+export default wrappedUserForm;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import ReactDom from 'react-dom';
+import UserForm from './UserForm'
+
+ReactDom.render(<UserForm />, document.getElementById('root'));
